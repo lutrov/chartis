d2ce7a3e6bab01a175581d9286a941b8871a7054
Ivan Lutrov <ivanlutrov@gmail.com>
2016-03-21 10:39:32 +1100

    Initial commit

--- chartis.php
+++ chartis.php
@@ -2,9 +2,9 @@
 
 /*
 Plugin Name: Chartis
-Description: Ensure that search engines know about all the <em>pages</em>, <em>posts</em> and <em>products</em> on your site with this simple, dynamic XML sitemap generator. Please note that this plugin doesn't create a robots.txt file. You should be adding your XML sitemap URL via Google and Bing Webmaster Tools instead. Why this plugin name? Chartis means "map" in Greek.
+Description: Ensure that search engines know about all the <em>pages</em>, <em>posts</em> and <em>custom post types</em> on your site with this simple, dynamic XML sitemap generator. Please note that this plugin doesn't create a robots.txt file. You should be adding your XML sitemap URL via Google and Bing Webmaster Tools instead. Why this plugin name? Chartis means "map" in Greek.
 Author: Ivan Lutrov
-Version: 1.3
+Version: 1.4
 Author URI: http:// lutrov.com/
 */
 
@@ -21,17 +21,19 @@ function chartis_sitemap() {
 	$result = sprintf("%s<changefreq>daily</changefreq>\n", $result);
 	$result = sprintf("%s<priority>1</priority>\n", $result);
 	$result = sprintf("%s</url>\n", $result);
-	$fp = (int) get_option('page_on_front');
-	$sql = sprintf("SELECT ID, post_type, post_title, post_modified_gmt FROM %s WHERE ID <> %s AND post_type IN('page', 'post', 'portfolio', 'product') AND post_status = 'publish' AND post_password = '' ORDER BY post_type ASC, post_modified DESC", $wpdb->posts, $fp);
+	$sql = sprintf("SELECT ID, post_type, post_title, post_modified_gmt FROM %s WHERE ID <> %s AND post_type IN(%s) AND post_status = 'publish' AND post_password = '' ORDER BY post_type ASC, post_modified DESC", $wpdb->posts, (int) get_option('page_on_front'), chartis_get_post_types());
 	$posts = $wpdb->get_results($sql);
 	foreach ($posts as $post) {
 		if (strlen($post->post_title) > 0) {
-			$result = sprintf("%s<url>\n", $result);
-			$result = sprintf("%s<loc>%s</loc>\n", $result, get_permalink($post->ID));
-			$result = sprintf("%s<lastmod>%s</lastmod>\n", $result, mysql2date('Y-m-d\TH:i:s+00:00', $post->post_modified_gmt, false));
-			$result = sprintf("%s<changefreq>weekly</changefreq>\n", $result);
-			$result = sprintf("%s<priority>0.8</priority>\n", $result);
-			$result = sprintf("%s</url>\n", $result);
+			$permalink = get_permalink($post->ID);
+			if (strpos($permalink, '?') === false) {
+				$result = sprintf("%s<url>\n", $result);
+				$result = sprintf("%s<loc>%s</loc>\n", $result, $permalink);
+				$result = sprintf("%s<lastmod>%s</lastmod>\n", $result, mysql2date('Y-m-d\TH:i:s+00:00', $post->post_modified_gmt, false));
+				$result = sprintf("%s<changefreq>weekly</changefreq>\n", $result);
+				$result = sprintf("%s<priority>0.8</priority>\n", $result);
+				$result = sprintf("%s</url>\n", $result);
+			}
 		}
 	}
 	$result = sprintf("%s</urlset>", $result);
@@ -40,6 +42,25 @@ function chartis_sitemap() {
 	header('Content-Type: text/xml');
 	printf("%s\n", $result);
 	exit();
+}
+
+//
+// Get all relevant post types.
+//
+function chartis_get_post_types() {
+	$result = null;
+	$skip = array('attachment', 'nav_menu_item', 'revision');
+	$types = get_post_types(null, 'names');
+	foreach ($types as $type) {
+		if (in_array($type, $skip) == false) {
+			if (strlen($result) > 0) {
+				$result = sprintf("%s, '%s'", $result, $type);
+			} else {
+				$result = sprintf("'%s'", $type);
+			}
+		}
+	}
+	return $result;
 }
 
 //
