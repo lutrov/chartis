919390de0a354fc0571587cfb28fa94624f1aa46
Ivan Lutrov <lutrov@users.noreply.github.com>
2017-03-28 16:54:07 +1100

    Add files via upload
    Introduce filter to handle custom post types.

--- chartis.php
+++ chartis.php
@@ -2,16 +2,20 @@
 
 /*
 Plugin Name: Chartis
-Description: Ensure that search engines know about all the <em>pages</em>, <em>posts</em> and <em>custom post types</em> on your site with this simple, dynamic XML sitemap generator. Please note that this plugin doesn't create a robots.txt file. You should be adding your XML sitemap URL via Google and Bing Webmaster Tools instead. Why this plugin name? Chartis means "map" in Greek.
+Description: Ensure that search engines know about all the <em>pages</em>, <em>posts</em> and <em>custom post types</em> on your site with this simple, dynamic XML sitemap generator. Why this plugin name? Chartis means "map" in Greek.
 Author: Ivan Lutrov
-Version: 1.4
+Version: 2.0
 Author URI: http:// lutrov.com/
+Notes: This plugin provides a filter to modify the post types to include in the sitemap. See this example:
+// Add "movie", "book" and "product" custom post types.
+add_filter('chartis_post_types_filter', 'lutrov_chartis_post_types_filter', 10);
+function lutrov_chartis_post_types_filter($types) {
+	foreach (array('movie', 'book', 'product') as $type) {
+		array_push($types, $type);
+	}
+	return $types;
+}
 */
-
-//
-// Constants used by this plugin.
-//
-define('CHARTIS_POST_TYPES', 'post|page|product|course');
 
 //
 //  Dynamically generate XML sitemap.
@@ -26,19 +30,17 @@ function chartis_sitemap() {
 	$result = sprintf("%s<changefreq>daily</changefreq>\n", $result);
 	$result = sprintf("%s<priority>1</priority>\n", $result);
 	$result = sprintf("%s</url>\n", $result);
-	$sql = sprintf("SELECT ID, post_type, post_title, post_modified_gmt FROM %s WHERE ID <> %s AND post_type IN (%s) AND post_status = 'publish' AND post_password = '' ORDER BY post_type ASC, post_modified DESC", $wpdb->posts, (int) get_option('page_on_front'), sprintf("'%s'", str_replace('|', "', '", CHARTIS_POST_TYPES)));
+	$fp = (int) get_option('page_on_front');
+	$sql = sprintf("SELECT ID, post_type, post_title, post_modified_gmt FROM %s WHERE ID <> %s AND post_type IN(%s) AND post_status = 'publish' AND post_password = '' ORDER BY post_type ASC, post_name ASC", $wpdb->posts, $fp, chartis_post_types());
 	$posts = $wpdb->get_results($sql);
 	foreach ($posts as $post) {
 		if (strlen($post->post_title) > 0) {
-			$permalink = get_permalink($post->ID);
-			if (strpos($permalink, '?') === false) {
-				$result = sprintf("%s<url>\n", $result);
-				$result = sprintf("%s<loc>%s</loc>\n", $result, $permalink);
-				$result = sprintf("%s<lastmod>%s</lastmod>\n", $result, mysql2date('Y-m-d\TH:i:s+00:00', $post->post_modified_gmt, false));
-				$result = sprintf("%s<changefreq>weekly</changefreq>\n", $result);
-				$result = sprintf("%s<priority>0.8</priority>\n", $result);
-				$result = sprintf("%s</url>\n", $result);
-			}
+			$result = sprintf("%s<url>\n", $result);
+			$result = sprintf("%s<loc>%s</loc>\n", $result, get_permalink($post->ID));
+			$result = sprintf("%s<lastmod>%s</lastmod>\n", $result, mysql2date('Y-m-d\TH:i:s+00:00', $post->post_modified_gmt, false));
+			$result = sprintf("%s<changefreq>daily</changefreq>\n", $result);
+			$result = sprintf("%s<priority>0.8</priority>\n", $result);
+			$result = sprintf("%s</url>\n", $result);
 		}
 	}
 	$result = sprintf("%s</urlset>", $result);
@@ -47,6 +49,14 @@ function chartis_sitemap() {
 	header('Content-Type: text/xml');
 	printf("%s\n", $result);
 	exit();
+}
+
+//
+// Get post types to include.
+//
+function chartis_post_types() {
+	$types = apply_filters('chartis_post_types_filter', array('page', 'post'));
+	return sprintf("'%s'", implode("', '", $types));
 }
 
 //
