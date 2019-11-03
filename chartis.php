248de62be14407918c47e2dd56e2f53982d2552c
Ivan Lutrov <lutrov@users.noreply.github.com>
2019-11-04 09:36:44 +1100

    Update chartis.php

--- chartis.php
+++ chartis.php
@@ -6,17 +6,32 @@ Description: Ensure that search engines know about all the <em>pages</em>, <em>p
 Plugin URI: https://github.com/lutrov/chartis
 Author: Ivan Lutrov
 Author URI: http:// lutrov.com/
-Version: 2.1
+Version: 3.0
 Notes: This plugin provides an API to customise the default constant values and control the post types to include in the sitemap. See the "readme.md" file for more.
 */
 
 defined('ABSPATH') || die('Ahem.');
 
 //
+// Invoke plugin if XML sitemap requested.
+//
+add_action('plugins_loaded', 'chartis_init_action', 20);
+function chartis_init_action() {
+	if (preg_match('#/sitemap\.xml$#', $_SERVER['REQUEST_URI']) == 1) {
+		add_action('template_redirect', 'chartis_sitemap_action', 20);
+	}
+}
+
+//
 //  Dynamically generate XML sitemap.
 //
-function chartis_sitemap() {
-	global $wpdb;
+function chartis_sitemap_action() {
+	$posts = get_posts(array(
+		'post_type' => apply_filters('chartis_post_types_filter', array('page', 'post')),
+		'orderby' => array('post_type' => 'ASC', 'title' => 'ASC'),
+		'exclude' => (int) get_option('page_on_front'),
+		'posts_per_page' => -1
+	));
 	$result = sprintf("%s<?xml version=\"1.0\" encoding=\"%s\"?>\n", null, get_bloginfo('charset'));
 	$result = sprintf("%s<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd\">\n", $result);
 	$result = sprintf("%s<url>\n", $result);
@@ -25,17 +40,16 @@ function chartis_sitemap() {
 	$result = sprintf("%s<changefreq>daily</changefreq>\n", $result);
 	$result = sprintf("%s<priority>1</priority>\n", $result);
 	$result = sprintf("%s</url>\n", $result);
-	$fp = (int) get_option('page_on_front');
-	$query = sprintf("SELECT ID, post_type, post_title, post_modified_gmt FROM %s WHERE ID <> %s AND post_type IN (%s) AND post_status = 'publish' AND post_password = '' ORDER BY post_type ASC, post_name ASC", $wpdb->posts, $fp, chartis_post_types());
-	$posts = $wpdb->get_results($query);
-	foreach ($posts as $post) {
-		if (strlen($post->post_title) > 0) {
-			$result = sprintf("%s<url>\n", $result);
-			$result = sprintf("%s<loc>%s</loc>\n", $result, get_permalink($post->ID));
-			$result = sprintf("%s<lastmod>%s</lastmod>\n", $result, mysql2date('Y-m-d\TH:i:s+00:00', $post->post_modified_gmt, false));
-			$result = sprintf("%s<changefreq>daily</changefreq>\n", $result);
-			$result = sprintf("%s<priority>0.8</priority>\n", $result);
-			$result = sprintf("%s</url>\n", $result);
+	if (count($posts) > 0) {
+		foreach ($posts as $post) {
+			if (strlen($post->post_title) > 0) {
+				$result = sprintf("%s<url>\n", $result);
+				$result = sprintf("%s<loc>%s</loc>\n", $result, get_permalink($post->ID));
+				$result = sprintf("%s<lastmod>%s</lastmod>\n", $result, mysql2date('Y-m-d\TH:i:s+00:00', $post->post_modified_gmt, false));
+				$result = sprintf("%s<changefreq>daily</changefreq>\n", $result);
+				$result = sprintf("%s<priority>0.8</priority>\n", $result);
+				$result = sprintf("%s</url>\n", $result);
+			}
 		}
 	}
 	$result = sprintf("%s</urlset>", $result);
@@ -44,14 +58,6 @@ function chartis_sitemap() {
 	header('Content-Type: text/xml');
 	echo sprintf("%s\n", $result);
 	exit();
-}
-
-//
-// Get post types to include.
-//
-function chartis_post_types() {
-	$types = apply_filters('chartis_post_types_filter', array('page', 'post'));
-	return sprintf("'%s'", implode("', '", $types));
 }
 
 //
@@ -88,13 +94,6 @@ function chartis_activate() {
 register_deactivation_hook(__FILE__, 'chartis_deactivate');
 function chartis_deactivate() {
 	chartis_robots_textfile('uninstall');
-}
-
-//
-// Invoke plugin if XML sitemap requested.
-//
-if (preg_match('#/sitemap\.xml$#', $_SERVER['REQUEST_URI']) == 1) {
-	add_action('template_redirect', 'chartis_sitemap');
 }
 
 ?>
